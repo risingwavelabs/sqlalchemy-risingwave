f9e216af7b078d56dc1bb8686419dc594fd10577
Kexiang Wang <kx.wang@hotmail.com>
2024-01-10 16:47:28 -0500

    chore(version): support newer python version

--- setup.py
+++ setup.py
@@ -23,10 +23,11 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
     keywords="SQLAlchemy RisingWave",
     project_urls={
